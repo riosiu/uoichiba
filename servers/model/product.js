3f8aeec5b646aa6fc7f9f1f1fb93cc0a9f5b5d40
richard <richardo.aog@gmail.com>
2023-04-01 22:39:18 +0700

    first commit

---
+++ servers/model/product.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = models("Fish", productSchema);
+
+export default Product;
---
+++ servers/model/user.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = models("User", userSchema);
+export default User;
